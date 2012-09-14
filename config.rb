3180d3a8d0a5285e4b5d2aa89238388daee8c913
javi <qualopec@gmail.com>
2012-09-14 12:07:27 +0200

    first website version

---
+++ config.rb
@@ -0,0 +1,17 @@
+#
+# Compass configuration file
+#
+#line_comments = false;
+#relative_assets = true;
+
+http_path = "/"
+project_path = "."
+
+css_dir      = "output/stylesheets/"
+sass_dir     = "content/stylesheets/"
+images_dir   = "output/images/"
+output_style = :compact
+
+sass_options = {
+   :syntax => :scss
+}
