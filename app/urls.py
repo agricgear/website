2945d49455e866811bfa6b9b806d606249a56ec8
javi <qualopec@gmail.com>
2012-09-14 20:56:15 +0200

    djangp app working

--- app/urls.py
+++ app/urls.py
@@ -1,16 +1,24 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^', include('web.urls')),
+    url(r'', include('web.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('django.contrib.flatpages.urls')),
 )
+"""
+urlpatterns += i18n_patterns('',
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home-gps.html'}, name='home'),
+    url(_(r'^pc-gps-workout'), 'django.views.generic.simple.direct_to_template', {'template': 'home-pc.html'},  name='home-pc')
+)
+"""
--- web/urls.py
+++ web/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import ugettext_lazy as _
 
 urlpatterns = patterns('',
-    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home-gps.html'})
+    url(r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home-gps.html'}, name='home'),
+    url(_(r'^agroguia-pc'), 'django.views.generic.simple.direct_to_template', {'template': 'home-pc.html'},  name='home-pc'),
+    url(_(r'^comprar-guiado-gps'), 'django.views.generic.simple.direct_to_template', {'template': 'buy.html'},  name='home-buy')
 )
