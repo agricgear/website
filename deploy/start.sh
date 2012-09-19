9f71444c5f531b70adc3ab6d6f6ad15a0a850fc0
javi <qualopec@gmail.com>
2012-09-19 23:48:56 +0200

    deploy fix

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,7 +3,7 @@
 mkdir run
 
 echo " * sync database"
-cd ./env/bin/python manage.py syncdb && ./env/bin/python manage.py migrate && ./env/bin/gunicorn_django -D --pid=run/django.pid  -b 0.0.0.0:8182
+./env/bin/python manage.py syncdb && ./env/bin/python manage.py migrate && ./env/bin/gunicorn_django -D --pid=run/django.pid  -b 0.0.0.0:8182
 
 echo "====================== agroguia web deployed ========================"
 
--- web/admin.py
+++ web/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from models import Contact
 
 class ContactAdmin(admin.ModelAdmin): 
-    list_filter = ('date')
+    list_filter = ('date',)
     list_display = ('date', 'name', 'email', 'phone')
 
-admin.site.register(Work, WorkAdmin)
+admin.site.register(Contact, ContactAdmin)
