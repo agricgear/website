cb46736e336237360bbcb553695a0cc0bd255f56
javi <qualopec@gmail.com>
2012-09-20 23:36:51 +0200

    stop script

--- deploy/stop.sh
+++ deploy/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-cd tr/ag_tracker && kill -9 `cat run/django.pid` || echo "django isn't running"
+kill -9 `cat run/django.pid` || echo "django isn't running"
 echo "=================== Agrotrack stopped ===================="
