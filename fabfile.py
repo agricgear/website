05c4d17da79152e30f6dcc5ded4db272f70e0730
javi <qualopec@gmail.com>
2013-05-19 19:31:31 +0200

    script to include flatpages data in po files

--- fabfile.py
+++ fabfile.py
@@ -4,6 +4,9 @@ from fabric.api import env, run, local, settings, sudo
 from fabric.operations import get
 from secret import *
 from fabric.operations import get
+import json
+import os
+from os.path import basename, dirname
 
 def production():
   env.hosts = ['track.agroguia.es:2223']
@@ -26,5 +29,34 @@ def deploy_web():
 def backup():
     copy_database()
 
+
+FLATPAGE_TMPL = u"""
+<title>{%% blocktrans %%}%(title)s{%% endblocktrans %%} </title>
+<body>
+    {%% blocktrans %%}
+    %(content)s
+    {%% endblocktrans %%}
+</body>
+"""
+def render_flatpages(json_export_file):
+    pages = json.loads(open(json_export_file).read())
+    for p in pages:
+        page = p['fields']
+        try:
+            os.makedirs('templates' + dirname(page['url']))
+        except OSError:
+            pass
+        fname = 'templates' + page['url'] + ".html"
+        print "writting " + fname
+        content = FLATPAGE_TMPL % { 
+            'content': unicode(page['content']),
+            'title': unicode(page['title'])
+        }
+        open(fname, 'w').write(content.encode('utf-8'))
+
 def makemessages():
+    # dump data from flatpages
+    local('python manage.py dumpdata flatpages | python -mjson.tool  > /tmp/1.json')
+    # render 1.json temporally pages
+    render_flatpages('/tmp/1.json')
     local('django-admin.py makemessages --ignore env -l en')
