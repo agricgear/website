7547710f99032c3bede5ca9ce9a4d7f957c9976c
javi <qualopec@gmail.com>
2013-05-26 18:44:03 +0200

    deploy english

--- deploy/start.sh
+++ deploy/start.sh
@@ -3,7 +3,7 @@
 mkdir run
 
 echo " * sync database"
-./env/bin/python manage.py syncdb && ./env/bin/python manage.py migrate && ./env/bin/gunicorn_django -D --pid=run/django.pid  -b 0.0.0.0:8182 && ./env/bin/gunicorn_django --settings app.settings_en -D --pid=run/django.pid  -b 0.0.0.0:8183
+./env/bin/python manage.py syncdb && ./env/bin/python manage.py migrate && ./env/bin/gunicorn_django -D --pid=run/django.pid  -b 0.0.0.0:8182 && ./env/bin/gunicorn_django --settings app.settings_en -D --pid=run/django.en.pid  -b 0.0.0.0:8183 --pythonpath ~/agroguia-website
 
 echo "====================== agroguia web deployed ========================"
 
